e7f8dfffcf7136753799b882a12bd0f076dcfb01
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-05-14 16:45:36 +0700

    create virtual terminal receipt page and function

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,7 +11,11 @@ func (app *application) routes() http.Handler {
 	mux.Use(SessionLoad)
 
 	mux.Get("/", app.Home)
+
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/virtual-terminal-payment-succeeded", app.VirtualTerminalPaymentSucceeded)
+	mux.Get("/virtual-terminal-receipt", app.VirtualTerminalReceipt)
+
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 	mux.Get("/receipt", app.Receipt)
 	mux.Get("/widget/{id}", app.ChargeOnce)
