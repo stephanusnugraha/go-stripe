533813ca1d32d1298de55ca2512430d4eab577ce
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-05-16 14:34:37 +0700

    Create invalidCredentials & GetUserByEmail functions

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -254,6 +254,12 @@ func (app *application) CreateAuthToken(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
+	user, err := app.DB.GetUserByEmail(userInput.Email)
+	if err != nil {
+		app.invalidCredentials(w)
+		return
+	}
+
 	var payload struct {
 		Error   bool   `json:"error"`
 		Message string `json:"message"`
